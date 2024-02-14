c699ac8b8f97da784f1dcb76c4bf4544fe3ee0fa
SWizzard779 <sofia.nesimko@gmail.com>
2024-02-14 20:26:07 +0100

    tasks

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,31 @@
-function getElementWidth(content, padding, border) {
-    const elementContent = Number.parseFloat(content);
-    const elementPadding = Number.parseFloat(padding);
-    const elementBorder = Number.parseFloat(border);
+class StringBuilder {
+  #value;
+  constructor(value) {
+    this.#value = value;
+  }
 
-    const elementWidth = elementContent + elementPadding*2 + elementBorder*2;
-    return elementWidth
+  getValue() {
+    return this.#value;
+  }
+
+  padEnd(str) {
+    this.#value += str;
+  }
+
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+
+  padBoth(str) {
+    this.#value = str + this.#value + str;
+  }
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+const builder = new StringBuilder('.');
+console.log(builder.getValue()); // "."
+builder.padStart('^');
+console.log(builder.getValue()); // "^."
+builder.padEnd('^');
+console.log(builder.getValue()); // "^.^"
+builder.padBoth('=');
+console.log(builder.getValue()); // "=^.^="
